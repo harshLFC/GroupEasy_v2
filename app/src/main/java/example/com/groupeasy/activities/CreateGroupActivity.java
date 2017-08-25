a750c1788c23695ac53be5f5fba724b20ab779c2
harsh_elnino <d16123828@mydit.ie>
2017-08-25 13:18:31 +0100

    No crashes, Group data populating list view, Group pushing data

--- app/src/main/java/example/com/groupeasy/activities/CreateGroupActivity.java
+++ app/src/main/java/example/com/groupeasy/activities/CreateGroupActivity.java
@@ -8,15 +8,28 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import example.com.groupeasy.R;
+import example.com.groupeasy.pojo.chatPOJO;
 
 public class CreateGroupActivity extends AppCompatActivity {
 
     private Context context;
     private ImageView ivClose, groupDP;
+    private TextView next;
+    private EditText input;
+
+    //initilize elements
+    //database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final DatabaseReference myRef = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +40,8 @@ public class CreateGroupActivity extends AppCompatActivity {
 //        setSupportActionBar(toolbar);
         initElementsWithIds();
         initElementsWithListeners();
+
+
     }
 
     private void initElementsWithListeners() {
@@ -43,8 +58,35 @@ public class CreateGroupActivity extends AppCompatActivity {
         groupDP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
                 Toast.makeText(context, "Will open gallery",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        final DatabaseReference groupRef = myRef.child("Groups").child("");
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String groupName = input.getText().toString();
+                String members = null;
+                String icon = null;
+
+                if(groupName.isEmpty()){
+                }
+// else the entered string will be pushed to the firebase database reference
+                else {
+                }
+
+                chatPOJO chatPOJO = new chatPOJO(groupName, members,icon);
+
+                groupRef.push().setValue(chatPOJO);
+
+                Intent intent = new Intent(context,DashboardActivity.class);
+                startActivity(intent);
+                finish();
+
             }
         });
     }
@@ -52,6 +94,8 @@ public class CreateGroupActivity extends AppCompatActivity {
     private void initElementsWithIds() {
         ivClose = (ImageView) findViewById(R.id.iv_close);
         groupDP = (ImageView) findViewById(R.id.GroupDPImageView);
+        next = (TextView) findViewById(R.id.next);
+        input = (EditText) findViewById(R.id.input);
     }
 
     @Override
