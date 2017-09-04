530cd93f3600a7efe54cee905f76710eb31ab715
harsh_elnino <d16123828@mydit.ie>
2017-09-04 12:14:53 +0100

    No crashes, Group data populating list view, Group pushing data. Onclick listener added to RecyclerView Adapter class, Login done, logout done

--- app/src/main/java/example/com/groupeasy/editProfileActivity.java
+++ app/src/main/java/example/com/groupeasy/editProfileActivity.java
@@ -2,12 +2,14 @@ package example.com.groupeasy;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,6 +20,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import example.com.groupeasy.activities.DashboardActivity;
@@ -32,6 +36,8 @@ public class editProfileActivity extends AppCompatActivity {
 
     private DatabaseReference mStatusDatabase;
     private FirebaseUser mCurrentUser;
+
+    private static final int GALLERY_PICK = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +80,8 @@ public class editProfileActivity extends AppCompatActivity {
                     String Status = userStatus.getEditText().getText().toString();
                     String Name = userName.getEditText().getText().toString();
 
+                //code for updating username and status
+                //// TODO: 04-09-2017 use different ? loop or switch statement
                 if(!TextUtils.isEmpty(Status)){
                     mStatusDatabase.child("status").setValue(Status).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
@@ -132,6 +140,31 @@ public class editProfileActivity extends AppCompatActivity {
                 }
             }
         });
+
+        editImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(editProfileActivity.this);
+
+                //trying for button effect
+                //// TODO: 04-09-2017 delete if not required
+                buttonEffect(editImage);
+
+
+                /*
+                Intent gallery_intent = new Intent();
+                gallery_intent.setType("image/*");
+                gallery_intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(gallery_intent, "Select Image") ,GALLERY_PICK);
+                */
+
+
+            }
+        });
     }
 
     private void initElementsWithIds() {
@@ -141,5 +174,26 @@ public class editProfileActivity extends AppCompatActivity {
             saveChanges = (Button) findViewById(R.id.save_changes);
 
             editImage = (CircleImageView) findViewById(R.id.edit_dp);
+    }
+
+    public static void buttonEffect(View button){
+        button.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        v.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        v.getBackground().clearColorFilter();
+                        v.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
     }
 }
